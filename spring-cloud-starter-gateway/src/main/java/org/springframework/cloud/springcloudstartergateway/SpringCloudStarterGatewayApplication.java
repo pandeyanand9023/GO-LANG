875e770bd36a998621d4f1aa881ad543251efb3d
pandeyanand9023 <pandeyanand9023@gmail.com>
2021-07-17 00:19:59 +0530

    second push

---
+++ spring-cloud-starter-gateway/src/main/java/org/springframework/cloud/springcloudstartergateway/SpringCloudStarterGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.springframework.cloud.springcloudstartergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudStarterGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStarterGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-starter-gateway/src/test/java/org/springframework/cloud/springcloudstartergateway/SpringCloudStarterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.cloud.springcloudstartergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStarterGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
